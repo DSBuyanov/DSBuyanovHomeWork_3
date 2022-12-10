df290ac6dbb17e1cd3e8c3dbe4e7d78ffa2ed234
Denis <inquis87@gmail.com>
2022-12-10 18:42:00 +0300

    finish

--- task3/Program.cs
+++ task3/Program.cs
@@ -9,7 +9,7 @@ int[] GetNewArry(int[] arry)
 {
     for (int i = 0; i < arry.Length; i++)
     {
-        Console.Write($"Введите элемент массива по индексом {i}:\t ");
+        Console.Write($"Введите элемент массива под индексом {i}:\t ");
         arry[i] = int.Parse(Console.ReadLine());
     }
     for (int i = 0; i < arry.Length; i++)
@@ -18,21 +18,5 @@ int[] GetNewArry(int[] arry)
     }
     return arry;
 }
-void Skobk(int[] coll)
-{
-    int count = coll.Length;
-    int index = 0;
-    Console.Write("[");
-    while (index < count)
-    {
-        Console.Write(coll[index]);
-        index++;
-        if (index < count)
-        {
-            Console.Write(", ");
-        }
-    }
-    Console.Write("]");
-}
-Skobk(arry);
+GetNewArry(arry);
 
