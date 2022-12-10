1479196462ac950b8fd6bf18034a519263abe470
Denis <inquis87@gmail.com>
2022-12-10 18:34:33 +0300

    test

--- task3/Program.cs
+++ task3/Program.cs
@@ -18,6 +18,21 @@ int[] GetNewArry(int[] arry)
     }
     return arry;
 }
-GetNewArry(arry);
-
+void Skobk(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    Console.Write("[");
+    while (index < count)
+    {
+        Console.Write(coll[index]);
+        index++;
+        if (index < count)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+Skobk(arry);
 
