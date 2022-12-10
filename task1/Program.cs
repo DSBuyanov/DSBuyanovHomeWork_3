6a2d02a0a4815d7db7f5cbf6cd897304741fdff6
Denis <inquis87@gmail.com>
2022-12-10 14:13:04 +0300

    1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int NaturalStep(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result *= numberA;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int naturalStep = NaturalStep(numberA, numberB);
+Console.WriteLine(naturalStep);
